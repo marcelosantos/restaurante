ed9d7bb2435bc05a392adfae7dfc26ca156b1913
Marcelo Santos <marcelosantosadm@gmail.com>
2017-01-12 15:59:14 -0300

    arquivos do projeto

---
+++ app/controllers/orders.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+    "github.com/revel/revel"
+)
+
+type Orders struct {
+	*revel.Controller
+}
+
+func (c Orders) Create() revel.Result {
+	return c.Render()
+}
+
+func (c Orders) Payment() revel.Result {
+	return c.Render()
+}
+
+func (c Orders) GetPayment(orderId int) revel.Result {
+    println("Id order : ", orderId)
+	return c.RenderTemplate("orders/payment.html")
+}
